040dd41ae8bc3964704dada44c522bbe606673bd
Leandro Souza <31319438+leandrosouzaxd@users.noreply.github.com>
2020-05-18 23:09:43 -0300

    Create verificando grants.sql

---
+++ verificando grants.sql
@@ -0,0 +1,10 @@
+--SELECT VERIFICA GRANST --
+select * from (
+select 'GRANT '||privilege||' TO '||grantee||';' from dba_sys_privs
+where grantee in ('USUARIO1','USUARIO2')
+union all
+select 'grant '||privilege||' on '||grantor||'.'||table_name||' to '||grantee||';' from dba_tab_privs
+where grantee in ('USUARIO1','USUARIO2')
+union all
+select 'GRANT '||GRANTED_ROLE||' TO '||grantee||';' from dba_role_privs
+where grantee in ('USUARIO1','USUARIO2'));
